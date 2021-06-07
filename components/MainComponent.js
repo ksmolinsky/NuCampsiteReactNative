3113bb91c50d9c8bf7590ab165a7473b7222431e
Kyle Smolinsky <kyle.smolinsky@gmail.com>
2021-06-07 06:12:54 -0600

    week 4 workshop assignment

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -334,20 +334,25 @@ class Main extends Component {
         this.props.fetchPromotions();
         this.props.fetchPartners();
 
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios')
-                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
+        this.showNetInfo();
 
-        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
-            this.handleConnectivityChange(connectionInfo);
-        })
+        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => 
+            this.handleConnectivityChange(connectionInfo));
     }
 
     componentWillUnmount() {
         this.unsubscribeNetInfo();
+    }
+
+    showNetInfo = async () => {
+        
+        const connectionInfo = await NetInfo.fetch();
+
+            (Platform.OS === 'ios')
+                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                    connectionInfo.type, ToastAndroid.LONG);
+
     }
 
     handleConnectivityChange = connectionInfo => {
@@ -368,7 +373,7 @@ class Main extends Component {
         }
         (Platform.OS === 'ios')
             ? Alert.alert('Connection change:', connectionMsg)
-            : ToastAndroid.show(connectionMsg, ToastAndroid.LONG)
+            : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
     }
 
     render() {
